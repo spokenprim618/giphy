febd477cd91c454fd0858a53deb988fbcf181846
Glitch (global-bold-den) <none>
2022-04-13 22:30:26 +0000

    🏮😝 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-let customApiUrl = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
+$(".search-button").click(function () {
+  let input = $(".search-term").val();
+let customApiUrl =
+  `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
+  fetch(customApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
 
-$(".search-button").click(function() {
-    fetch(customApiUrl)
-        .then(function(response) {
-             return response.json();
-        })
+    .then(function (data) {
 
-        .then(function(data) {
-          return $("#img").attr('src', data.data[0].url);
-
-        });
-
+      return $("#img").attr("src", data.data[0].images.original.url);
+    
+    });
 });
