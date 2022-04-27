29ca9a27c628ec3241f46a5aafb246a57db5ed72
Glitch (global-bold-den) <none>
2022-04-27 22:27:36 +0000

    🤟📝 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 $(".search-button").click(function () {
   let input = $(".search-term").val();
-let customApiUrl =
-  `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
+  let input2 = $(".desired_amount").val();
+  let customApiUrl = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
   fetch(customApiUrl)
     .then(function (response) {
       return response.json();
     })
 
     .then(function (data) {
-
-      return $("#img").attr("src", data.data[0].images.original.url);
+      let randNum = Math.floor(Math.random() * data.data.length);
     
+    
+    
+  return $(".main").html(`<img src="${data.data[randNum].images.original.url}"/>`);
     });
 });
